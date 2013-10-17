03393acdcde6f00c1a8c7b89a75ab82cc0dad9c8
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-10-16 21:05:38 -0700

    Fetches websites on command!

--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,7 +1,19 @@
-exports.readUrls = function(filePath, cb){
+var fs = require('fs');
+var http = require('http-get');
+var sitesFile = __dirname + "/../../data/sites.txt";
+var sitesDir =  __dirname + "/../../data/sites/";
 
+exports.readUrls = function(){
+  var results = fs.readFileSync(sitesFile, 'utf8').split('\n');
+  return results;
 };
 
 exports.downloadUrls = function(urls){
-  // fixme
+  // to length -1 because assumed the last urls is empty
+  for (var i=0; i<urls.length-1; i++) {
+    http.get("http://"+urls[i],sitesDir+urls[i], function(err,result){
+      if(err) throw err;
+      console.log("success downloading url");
+    });
+  }
 };
