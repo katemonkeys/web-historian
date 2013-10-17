0fa016bde37dee1286462dce451a023e4b91aa18
Hack Reactor Students <github@hackreactor.com>
2013-10-16 19:14:37 -0700

    Handles POST requests to form, GET requests for websites in archive, and serves index/css/js files as appropriate

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,23 @@
+// var express = require("express");
+// var app = express();
+// var port = 8080;
+// var ip = "127.0.0.1";
+
+// app.configure(function(){
+//   app.use(
+//     "/",
+//     express.static(__dirname + "/public")
+//     );
+// });
+// app.listen(port, ip);
+
 var http = require("http");
-var myStuff = require("./request-handler");
+var handle = require("./request-handler");
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(myStuff.handleRequest);
+
+var server = http.createServer(handle.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,68 @@
 var path = require('path');
+var fs = require('fs');
+var headers = {};
+
+var mimeTypes = {
+  '.js' : 'text/javascript',
+  '.css' : 'text/css',
+  '.gif' : 'image/gif',
+  '.html': 'text/html'
+};
+
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
 
-module.exports.handleRequest = function (req, res) {
-  console.log(exports.datadir);
+module.exports.handleRequest = function (request, response) {
+  if (request.method === 'GET') {
+    handleGET(request, response);
+  }else if (request.method === 'POST') {
+    handlePOST(request, response);
+  } else {
+    var statusCode = 404;
+    response.writeHead(statusCode,headers);
+    console.log(exports.datadir);
+  }
+};
+
+
+//
+// ------------ GET ------------------
+//
+var handleGET = function(request, response){
+  var fileName = path.basename(request.url) || "index.html";
+  var fileExt = path.extname(request.url);
+  console.log("fileExt="+fileExt);
+  var fullPath;
+
+  if (fileName === "index.html" || fileExt === ".css" || fileExt === ".js") {
+    fullPath = __dirname+"/public/"+fileName;
+  } else {
+    fullPath = __dirname + "/../data/sites/" + fileName;
+  }
+  fs.readFile(fullPath, function(err, stuff){
+    if(!err){
+      headers['Content-Type'] = mimeTypes[fileExt];
+      response.writeHead(200,headers);
+      response.end(stuff);
+    }else{
+      console.log("there's a problem   ", err);
+    }
+  });
+};
+
+
+
+//
+// ------------ POST ------------------
+//
+var handlePOST = function(request, response){
+  console.log("recieved POST request");
+  var body = "";
+  request.on('data', function(data){
+    body += data;
+  });
+  request.on('end', function(){
+    console.log("something happened yo", body);
+    response.writeHead(201, headers);
+    response.end();
+  });
 };
