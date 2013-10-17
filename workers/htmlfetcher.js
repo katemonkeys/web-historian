91caa925e943c99334e2a94d289857f99111d0ea
Hack Reactor Students <github@hackreactor.com>
2013-10-16 21:05:38 -0700

    Fetches websites on command!

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,3 @@
-// eventually, you'll have some code here that uses the tested helpers 
-// to actually download the urls you want to download.
+var helpers = require(__dirname+'/lib/html-fetcher-helpers');
+var allURLs = helpers.readUrls();
+helpers.downloadUrls(allURLs);
